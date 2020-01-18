b8b9a27a9e00470616e3b094b8f942ae0e6ba0f5
Christopher Henderson <chrisrhenderson@outlook.com>
2020-01-18 16:16:40 -0700

    Create ASP.Net Solution With Basic EFDataAccessLibrary

---
+++ EFDemoApp/EFDataAccessLibrary/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDataAccessLibrary.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateofBirth { get; set; }
+
+        public List<Address> Addresses { get; set; }
+        public List<Email> EmailAddresses { get; set; }
+    }
+}
